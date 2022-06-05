021e914f81338d75daa09481f0a93341bc3e978b
StanEkso <90618842+StanEkso@users.noreply.github.com>
2022-06-05 15:53:42 +0300

    fix: cors-policy

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const cors = require('cors')
 
 const PORT = process.env.PORT || 8080;
 const app = express();
-
 const auth = require('./routes/auth.routes');
+app.use(cors());
 app.use(express.json())
 app.use('/auth', auth);
 
