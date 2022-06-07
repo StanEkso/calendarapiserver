921a06aa48313c6559fa3fc6635daa8488c3df4b
StanEkso <90618842+StanEkso@users.noreply.github.com>
2022-06-07 18:55:23 +0300

    feat: auth middleware to check jwt token

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,10 +1,10 @@
 const Router = require('express').Router;
-
+const authMiddleware = require('../middleware/auth.middleware')
 const router = new Router();
 const postController = require('../controllers/posts.controller')
 
-router.post('/create',postController.createPost);
+router.post('/create',authMiddleware,postController.createPost);
 router.get('/', postController.getPosts);
-router.delete('/:id',postController.deletePost);
-router.put('/', postController.updatePost);
+router.delete('/:id',authMiddleware,postController.deletePost);
+router.put('/',authMiddleware,postController.updatePost);
 module.exports = router;
