fe46e05444269d2b962deffeea3ba6d11a27f381
StanEkso <90618842+StanEkso@users.noreply.github.com>
2022-07-14 10:02:27 +0300

    fix: client.connect method

--- database.js
+++ database.js
@@ -1,6 +1,10 @@
 const pg = require('pg')
 // const { db } = require('./config')
 // const pool = new Pool(db)
-const client = new pg.Client(process.env.DATABASE_URL)
+const client = new pg.Client({connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }})
 
+client.connect();
 module.exports = client;
