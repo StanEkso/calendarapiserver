ea1e87ce360d89fd236fa2575b2a413c506fd220
StanEkso <90618842+StanEkso@users.noreply.github.com>
2022-07-14 10:13:36 +0300

    fix: port

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const express = require('express');
 const cors = require('cors')
 
 const PORT = process.env.PORT || 8080;
-console.log(process.env)
 const app = express();
 const auth = require('./routes/auth.routes');
 const posts = require('./routes/post.routes')
 const views = require('./routes/view.routes')
+
 app.use(cors());
 app.use(express.json({extended: true}))
 app.use('/posts',posts);
 app.use('/auth', auth);
 app.use('/views', views)
+
+console.log(process.env.DATABASE_PORT);
 
 app.listen(PORT, () => console.log("App started at port " + PORT))
