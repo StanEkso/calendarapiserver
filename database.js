76811407d7631ad7c73866246925ee7dd49c62a9
StanEkso <90618842+StanEkso@users.noreply.github.com>
2022-07-14 09:53:26 +0300

    feat: connection via creditianals

--- database.js
+++ database.js
@@ -1,5 +1,6 @@
-const Pool = require('pg').Pool;
-const { db } = require('./config')
-const pool = new Pool(db)
+const pg = require('pg')
+// const { db } = require('./config')
+// const pool = new Pool(db)
+const client = new pg.client(process.env.DATABASE_URL)
 
-module.exports = pool;
+module.exports = client;
